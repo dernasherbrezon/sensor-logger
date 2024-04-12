8363da8a7da3d75e9f7520274450e9bf97051056
dernasherbrezon <dernasherbrezon@gmail.com>
2024-04-12 13:18:16 +0100

    initial idea

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# sensor-logger
-Simple data logger focused on storing sensor information
+## About
+
+Data logger focused on storing sensor information. This library can also be used to read the data, so in some places "sensor logger" is also called "database".
+
+## Design
+
+It is very important to know what "sensor logger" IS and what IS NOT. While designing this library the following assumptions were made:
+
+ * Primary use is microcontrollers with SD card attached. It is possible to store in the internal NVM memory, but the application should manage retention on its own.
+ * Append-only. Current SD cards have capacity ~32Gb. Very rough estimations for 6 metrics 4 bytes each sampled every 10 minutes gives ~8000 years of data.
+ * Cross-platform database. SD card can be inserted into any other POSIX operating system and database can be read from it.
+ * Sensor-focused. ADC in the sensor is normally 8, 12 or 16bits. Thus, values can be maximum of 2 bytes.
+ * Data is sampled at the very low rate. Currently, these rates are fixed and are: 1min, 5min and 10min.
+ * Data is partitioned by day, month and year. This is mostly covering the basic use cases like: retrieve last day, month or year.
+
+## Metrics
+
+Metrics can be of 2 types:
+
+ * GAUGE - the next value doesn't depend on the previous. For example: temperature.
+ * COUNTER - the value is incrementing over time. Can overflow. For example: number of bytes sent.
+
+It is very important to understand the difference and select proper type. "GAUGE" and "COUNTER" are stored differently. For example, monthly or yearly aggregates calculate "GAUGE" as simple moving average (SMA).
+
+## File system structure
+
+Sensor logger is storing the data on the file system in the binary files. Each metric should be stored on its own base directory. The sample layout might look like this:
+
+```
+/root
+  /meta
+  /2024/
+    /data
+    /2024.01/
+      /data
+      /2024.01.01/
+        /data
+      /2024.01.02/
+        /data
+    /2024.02/
+      /data
+      /2024.02.01/
+```
+
+Where ```meta``` file is binary and contains database configuration.
+
+```data``` files are binary and store the actual data.
+
+
---
+++ include/slog.h
@@ -0,0 +1,104 @@
+#ifndef SENSOR_LOGGER_H
+#define SENSOR_LOGGER_H
+
+#include <stdint.h>
+#include <time.h>
+
+#define VERSION 1
+
+#define SLOG_OK 0
+#define SLOG_EMPTY 1
+#define SLOG_NON_EMPTY 2
+#define SLOG_FAIL 3
+
+typedef enum {
+  COUNTER = 0,
+  GAUGE = 1
+} slog_metric_type_t;
+
+typedef enum {
+  DAY = 0,
+  MONTH = 1,
+  YEAR = 2
+} slog_partition_t;
+
+typedef enum {
+  ONE_MINUTE = 0,   // 1440 samples per day
+  FIVE_MINUTES = 1, // 288 samples per day
+  TEN_MINUTES = 2   // 144 samples per day
+} slog_sample_interval_t;
+
+typedef enum {
+  LENGTH_8BITS = 0,
+  LENGTH_12BITS = 1,
+  LENGTH_16BITS = 2
+} slog_sample_size_t; // normally correspond to ADC resolution
+
+typedef struct {
+  uint8_t version;
+  slog_sample_interval_t sample_interval;
+  slog_metric_type_t metric_type;
+  slog_sample_size_t sample_size_bits;
+} header_t;
+
+typedef struct slog_t slog;
+
+/**
+ * @brief Initialize database.
+ *
+ * @param current - used to determine number of missing samples since the last "append" operation. Also used to calculate internally the next sample and partition after "append" operation.
+ * @param base_dir - base directory where metric partitions are stored. Each metric should have own root directory.
+ * @param result - handle to sensor logger. Used in any other operations.
+ * @return
+ *      - SLOG_OK - if initialization is success
+ *      - SLOG_EMPTY - if base directory is empty and database needs to be set up using "slog_setup" function
+ *      - SLOG_FAIL - on any other failure
+ */
+int slog_create(struct tm current, const char *base_dir, slog **result);
+
+/**
+ * @brief Setup database in the empty base directory.
+ *
+ * @param header - configuration
+ * @param handle - handle to the sensor logger
+ * @return
+ *      - SLOG_OK - if setup is success
+ *      - SLOG_NON_EMPTY - if base directory is not empty and already initialized
+ *      - SLOG_FAIL - on any other failure
+ */
+int slog_setup(header_t *header, slog *handle);
+
+/**
+ * @brief Append new value to the database. Recalculate monthly and yearly aggregates. Caller should ensure this function is called periodically according to the "slog_sample_interval_t" configuration.
+ *
+ * @param value - value to append.
+ * @param handle - handle to the sensor logger
+ * @return
+ *      - SLOG_OK - if setup is success
+ *      - SLOG_FAIL - on any other failure
+ */
+int slog_append(void value, slog *handle);
+
+/**
+ * @brief Read values from the database.
+ *
+ * @param partition - controls the data resolution. More granular resolution gives more data points.
+ * @param from - Select from time.
+ * @param to - Select to time.
+ * @param result - Output values into the pre-allocated array. Data type is determined by the "slog_sample_size_t".
+ * @param result_length - Number of data points in the result.
+ * @param handle - handle to the sensor logger
+ * @return
+ *      - SLOG_OK - if setup is success
+ *      - SLOG_FAIL - on any other failure
+ */
+int slog_read(slog_partition_t partition, struct tm from, struct tm to, void **result, uint8_t *result_length, slog *handle);
+
+/**
+ * @brief Destroy any internally allocated resources. Handle will become invalid after calling this function.
+ *
+ * @param handle
+ */
+void slog_destroy(slog *handle);
+
+#endif //SENSOR_LOGGER_H
